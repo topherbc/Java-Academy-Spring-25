0bc10617fb8e293c6d568a566fe7fe69dbbd82dc
Topher C <topherbyron@duck.com>
2025-04-11 13:52:33 -0400

    mid day 5 week 1

---
+++ week-01/class/day-5/SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
@@ -0,0 +1,45 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class SandwichShop {
+    public static void main(String[] args) {
+        Scanner waiter = new Scanner(System.in);
+        float regularSize = 5.45f;
+        float largeSize = 8.95f;
+
+        System.out.println("Welcome Big T's Sandwich Shoppe");
+
+        System.out.print("Please choose a sandwich size (Enter 1 for Regular, 2 for Large): ");
+
+        int option = waiter.nextInt();
+
+        System.out.print("Please enter your age for a discount: ");
+
+        int age = waiter.nextInt();
+
+        float base = 0f;
+
+        if (option == 1) {
+            base = regularSize;
+        } else if (option == 2) {
+            base = largeSize;
+        } else {
+            System.out.println("Please choose a correct option, 1 or 2; Please restart the app;");
+        }
+
+        double discount = 0.0;
+
+        if (age <= 17) {
+            discount = 0.10;
+        } else if (age >= 65) {
+            discount = 0.20;
+        }
+
+        System.out.printf("Your total is: $%.2f", base * (1-discount));
+
+
+
+
+    }
+}
