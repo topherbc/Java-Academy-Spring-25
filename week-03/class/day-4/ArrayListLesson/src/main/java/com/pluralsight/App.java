adc6e1ea44b38eef2dc197eaaf14c57e21165db5
Topher C <topherbyron@duck.com>
2025-04-24 10:37:04 -0400

    mid day 4 week 3

---
+++ week-03/class/day-4/ArrayListLesson/src/main/java/com/pluralsight/App.java
@@ -0,0 +1,46 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class App {
+    public static void main(String[] args) {
+//        String[] sesameStreetCharacters = {"Big Bird", "Elmo", "Cookie Monster", "Oscar"};
+
+//        System.out.println(sesameStreetCharacters[2]);
+
+//        sesameStreetCharacters[2] = "Grouch";
+
+        ArrayList<String> sesameStreetCharacters = new ArrayList<String>();
+
+        sesameStreetCharacters.add("Big Bird");
+        sesameStreetCharacters.add("Toph Bird");
+        sesameStreetCharacters.add("Oscar");
+        sesameStreetCharacters.add("Bert");
+        sesameStreetCharacters.add("Cookie Monster");
+        sesameStreetCharacters.add("Rocky");
+        sesameStreetCharacters.add("Count");
+
+        System.out.println(sesameStreetCharacters.get(4));
+        System.out.println(sesameStreetCharacters);
+
+        sesameStreetCharacters.set(1, "Ernie");
+
+        System.out.println(sesameStreetCharacters);
+
+        Collections.sort(sesameStreetCharacters);
+        Collections.fill(sesameStreetCharacters, "Big Bird");
+
+        System.out.println(sesameStreetCharacters);
+
+        sesameStreetCharacters.add("Zoey");
+        sesameStreetCharacters.add("Mr. Beaker");
+
+        sesameStreetCharacters.remove(6);
+
+        System.out.println(sesameStreetCharacters);
+
+
+
+    }
+}
---
+++ week-03/class/day-4/day-4-objectives.md
@@ -0,0 +1,38 @@
+What did you learn yesterday?
+
+1. BufferedReader
+2. LocalDateTime.now() - gets now
+3. Date Formatting
+4. BufferedWriter
+   1. .write() - writing to file
+   2. .close() - release resources, saves file
+5. try {} catch() {}
+   1. IOException - particular exception thrown by writer/reader
+6. System.exit(0) - closes application
+7. IO - Input/Ouput 
+8. Classes are UpperCamelCase
+9.  variables/methods are lowerCamelCase
+10. /* block comments, provide info when researching a method */
+11. Fixing syntax
+12. Loggers keep track of interactions within program 
+13. .csv - comma separated values
+14. .json - javascript object notation
+
+
+Today's Objectives
+
+1. Collections
+2. ArrayList
+3. HashMap
+
+Agenda Today
+
+1. Codewar warmup and recap
+2. Start collections
+3. Break
+4. Collections
+5. Lunch
+6. Collections
+7. Break
+8. Collections
+9. Recap and Q&A
